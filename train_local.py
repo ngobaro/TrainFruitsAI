0484776beea00918e407d33f512772e75e59d42e
ngobaro <ngobaocoder@gmail.com>
2026-04-27 15:53:41 +0700

    fix url train_local and data.yaml

--- train_local.py
+++ train_local.py
@@ -1,13 +1,16 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8s.pt")   # hoặc yolov8n.pt, yolov8m.pt
+# Train model
+model = YOLO("yolov8s.pt")        # yolov8n.pt nếu máy yếu, yolov8m.pt nếu muốn chính xác hơn
 
 results = model.train(
-    data="dataset/data.yaml",
+    data="dataset/data.yaml",     # đường dẫn tương đối
     epochs=60,
     imgsz=640,
-    batch=8,                 # giảm nếu máy yếu (4 hoặc 8)
-    name="fruit5_yolov8s",
+    batch=8,                      # Windows local: điều chỉnh theo VRAM của bạn
+    name="fruit4_yolov8s",
     patience=20,
-    plots=True
+    plots=True,
+    pretrained=True,
+    device=0                      # dùng GPU (nếu có), dùng 'cpu' nếu không có GPU
 )
